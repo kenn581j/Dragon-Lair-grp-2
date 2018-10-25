240b2b4e0a59dea44a4058b94332108e5db6245e
Anders Dalgsaard Madsen <ande48d0@edu.eal.dk>
2018-10-25 09:23:36 +0200

    GetMatch + IsMatchesFinished + beskrivende kommentarer.

--- TournamentLibrary/Round.cs
+++ TournamentLibrary/Round.cs
@@ -11,16 +11,37 @@ namespace TournamentLib
             matches.Add(m);
         }
 
+        /*
+        Bruger m som parameter til at søge hvert added match igennem i matches listen. 
+        Derefter sætter jeg hver opponentnavn = teamnavn. Hvis dette er true, så returnerer jeg m, hvis ikke, så null.
+        */
         public Match GetMatch(string teamName1, string teamName2)
         {
-            // TODO: Implement this method
+            foreach (Match m in matches)
+            {
+                if(m.FirstOpponent.Name == teamName1 && m.SecondOpponent.Name == teamName2)
+                {
+                    return m;
+                }
+            }
             return null;
         }
-
+        
         public bool IsMatchesFinished()
         {
-            // TODO: Implement this method
-            return false;
+            /*
+           Her laver jeg et foreach loop på samme måde som ovenover.
+           Jeg spørger om der er en vinder på det det valgte match.
+           Hvis m = null, altså at der ikke findes en winner, så skal den være false - ellers true.
+            */
+            foreach (Match m in matches)
+            {
+                if(m.Winner == null)
+                {
+                    return false;
+                }
+            }
+                return true;
         }
 
         public List<Team> GetWinningTeams()
