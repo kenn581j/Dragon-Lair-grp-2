b8b42d0a7958ecef3e92c393b81b5f81628ee87f
Anders Dalgsaard Madsen <ande48d0@edu.eal.dk>
2018-10-25 14:29:06 +0200

    ShowCase() Done - ikke kommenteret. (Snak om evt. bedre løsning med gruppe?)

--- DragonsLair/Controller.cs
+++ DragonsLair/Controller.cs
@@ -11,15 +11,48 @@ namespace DragonsLair
 
         public void ShowScore(string tournamentName)
         {
-            /*
-             * TODO: Calculate for each team how many times they have won
-             * Sort based on number of matches won (descending)
-             */
-            Tournament t;
-            t = tournamentRepository.GetTournament(tournamentName);
-            t.GetTeams()
+            Tournament tName = tournamentRepository.GetTournament(tournamentName);
+            Team[] allTeams = tName.GetTeams().ToArray();
+            int[] allScores = new int[allTeams.Length];
 
-            Console.WriteLine("Implement this method!");
+            for (int i = 0; i < tName.GetNumberOfRounds(); i++)
+            {
+                Round round = tName.GetRound(i);
+                List<Team> winningTeams = round.GetWinningTeams();
+                for (int teamI = 0; teamI < allTeams.Length; teamI++)
+                {
+                    for (int winningTeamI = 0; winningTeamI < winningTeams.Count; winningTeamI++)
+                    {
+                        if (allTeams[teamI].Name == winningTeams[winningTeamI].Name)
+                        {
+                            allScores[teamI]++;
+                        }
+                    }
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("0------------------------------------------------------0");
+            Console.WriteLine("|  #####                                               |");     
+            Console.WriteLine("| #     #  ######  #  #       #       #  #    #  ####  |");
+            Console.WriteLine("| #          #     #  #       #       #  ##   # #    # |");
+            Console.WriteLine("|  #####     #     #  #       #       #  # #  # #      |");
+            Console.WriteLine("|       #    #     #  #       #       #  #  # # #  ### |");
+            Console.WriteLine("| #     #    #     #  #       #       #  #   ## #    # |");
+            Console.WriteLine("|  #####     #     #  ######  ######  #  #    #  ####  |");
+            Console.WriteLine("0------------------------------------------------------0");
+            for (int num = allScores.Max(); num >= 0; num--)
+            {
+                for (int i = 0; i < allTeams.Length; i++)
+                {
+                    if (allScores[i] == num)
+                    {
+                        Console.WriteLine($"            Team: '{allTeams[i]}' - Score: {allScores[i]} point");
+                    }
+                }
+            }
+            Console.Write("\nTryk på en vilkårlig tast for, at vende tilbage til menuen. . .");
+            Console.ReadKey();
+            Console.Clear();
         }
 
         public void ScheduleNewRound(string tournamentName, bool printNewMatches = true)
